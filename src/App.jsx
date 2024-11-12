769c81cd5b715b66e7db70defa61ce2a5281e7ed
nayembwip <nayem@bwipholdings.com>
2024-11-12 18:54:15 +0600

    Day 1 - Learn about Fundamentals of Component and Reusable Components with Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Info from "./components/Info";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Info
+        name="Nayem"
+        email="nayem323@gmail.com"
+        mobile="5346456456"
+        hobbies={[
+          "Reading",
+          "Travelling",
+          "Listening to music",
+          "Watching movies",
+          "Playing games",
+        ]}
+      />
+      <hr />
+      <Info
+        name="Shuvo"
+        mobile="8756973245"
+        hobbies={["Travelling", "Listening to music", "Playing games"]}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
