597cb8515ecc72defdf6669052a28d45e149aa8d
nayembwip <nayem@bwipholdings.com>
2024-11-21 00:03:46 +0600

    Conditional rendering, Rendering List

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,80 @@
-import "./App.css";
-import Info from "./components/Info";
+// Conditional rendering starts here
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {/* {isPacked ? (
+        <>
+          <del>{name}</del>
+          <span> ✅</span>
+        </>
+      ) : (
+        name
+      )} */}
+      {name} {isPacked && "✅"}
+    </li>
+  );
+}
+// Conditional rendering ensds here
 
-function App() {
+export default function PackingList() {
+  // Rendering List starts here
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+    },
+    {
+      id: 3,
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+    },
+    {
+      id: 4,
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+    },
+  ];
+
+  const chemists = people.filter((people) => people.profession === "chemist");
+  // Rendering List ends here
   return (
     <>
-      <Info
-        name="Nayem"
-        email="nayem323@gmail.com"
-        mobile="5346456456"
-        hobbies={[
-          "Reading",
-          "Travelling",
-          "Listening to music",
-          "Watching movies",
-          "Playing games",
-        ]}
-      />
-      <hr />
-      <Info
-        name="Shuvo"
-        mobile="8756973245"
-        hobbies={["Travelling", "Listening to music", "Playing games"]}
-      />
+      <section>
+        <h2>Conditional rendering</h2>
+        <h3>Sally Ride's Packing List</h3>
+        <ul>
+          <Item isPacked={true} name="Space suit" />
+          <Item isPacked={true} name="Helmet with a golden leaf" />
+          <Item isPacked={false} name="Photo of Tam" />
+        </ul>
+      </section>
+      <br />
+      <br />
+      <section>
+        <h2>Rendering List</h2>
+        <ul>
+          {people.map((people) => (
+            <li key={people.id}>{people.name}</li>
+          ))}
+        </ul>
+        <h3>Chemists</h3>
+        <ul>
+          {chemists.map((chemist) => (
+            <li key={chemist.id}>{chemist.name}</li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
-
-export default App;
