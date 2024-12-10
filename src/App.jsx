4aed562aa480781782783f1e1119f42d164d7888
nayembwip <nayem@bwipholdings.com>
2024-12-10 16:17:37 +0600

    Use of useEffect in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+            .then((res) => res.json())
+            .then((data) => {
+                setPosts(data);
+            });
+    }, []);
+
     return (
         <div>
-            <h1>Welcome to React</h1>
+            <h1>Posts</h1>
+            <ul>
+                {posts?.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
